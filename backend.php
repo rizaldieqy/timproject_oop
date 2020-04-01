f0b47f283616381e0897f0154b6fdd1ab69fc157
rizaldieqy <rizalasshidieqy09@gmail.com>
2020-04-01 23:45:39 +0700

    revisi isi backend done

--- backend.php
+++ backend.php
@@ -5,13 +5,30 @@ abstract class Karyawan{
 }
 
 class stayHome extends Karyawan{
-    public $nik = 1001;
-    public $nama = "Jamal";
-    public $alamat = "Jl. Lurus";
-    protected $tanggungan = "1 istri & 1 anak";
-    protected $jumlahkendaraan = "2 Motor";
-    private $gaji = 3000;
+    public $nik, $nama, $alamat;
+    protected $tanggungan, $jumlahkendaraan;
+    private $gaji;
 
+
+    public function setNik($nik){
+        $this->nik = $nik;
+    }
+
+    public function setNama($nama){
+        $this->nama = $nama;
+    }
+
+    public function setAlamat($alamat){
+        $this->alamat = $alamat;
+    }
+
+    public function setTanggungan($tanggungan){
+        $this->tanggungan = $tanggungan;
+    }
+
+    public function setJumlahkendaraan($jumlahkendaraan){
+        $this->jumlahkendaraan = $jumlahkendaraan;
+    }
 
     public function setGaji($gaji){
         $this->gaji = $gaji;
@@ -27,18 +44,37 @@ class stayHome extends Karyawan{
 }
 
 $stayhome = new stayHome();
-echo $stayhome->hitungKaryawan();
+$karyawanstay = $stayhome->setNik = "1001" . " " . $stayhome->setNama = "Jamal" . " " . $stayhome->setAlamat = "Jl. Jalan" . " " . $stayhome->setTanggungan = "1 istri & 1 anak" . " " . $stayhome->setJumlahkendaraan = "2 motor" . " " . $stayhome->setGaji = "3000";
+echo $karyawanstay;
+
 
 
 
 class Wfh extends Karyawan{
-    public $nik = 1005;
-    public $nama = "Yadi";
-    public $alamat = "Jl. Panjang";
-    protected $tanggungan = "1 istri & 3 anak";
-    protected $jumlahkendaraan = "2 Motor & 1 Mobil";
-    private $gaji = 5000;
+    public $nik, $nama, $alamat;
+    protected $tanggungan, $jumlahkendaraan;
+    private $gaji;
 
+
+    public function setNik($nik){
+        $this->nik = $nik;
+    }
+
+    public function setNama($nama){
+        $this->nama = $nama;
+    }
+
+    public function setAlamat($alamat){
+        $this->alamat = $alamat;
+    }
+
+    public function setTanggungan($tanggungan){
+        $this->tanggungan = $tanggungan;
+    }
+
+    public function setJumlahkendaraan($jumlahkendaraan){
+        $this->jumlahkendaraan = $jumlahkendaraan;
+    }
 
     public function setGaji($gaji){
         $this->gaji = $gaji;
@@ -49,11 +85,12 @@ class Wfh extends Karyawan{
     }
 
     public function hitungKaryawan(){
-        return ($this->nik . " " . $this->nama . " " . $this->alamat . " " . $this->tanggungan . " " . $this->jumlahkendaraan . " " . $this->gaji);
+        return ($this->nik . " " . $this->nama . " " . $this->alamat . " " . $this->tanggungan . " " . $this->jumlahkendaraan . " " . $this->gaji . "<br>");
     }
 }
 
-$wfh = new Wfh();
-echo $wfh->hitungKaryawan();
+$workfromhome = new Wfh();
+$karyawanhome = $karyawanhome->setNik = "1005" . " " . $karyawanhome->setNama = "Yadi" . " " . $karyawanhome->setAlamat = "Jl. Lurus" . " " . $karyawanhome->setTanggungan = "1 istri & 3 anak" . " " . $karyawanhome->setJumlahkendaraan = "2 motor & 1 mobil" . " " . $karyawanhome->setGaji = "5000";
+echo $karyawanhome;
 
 ?>
